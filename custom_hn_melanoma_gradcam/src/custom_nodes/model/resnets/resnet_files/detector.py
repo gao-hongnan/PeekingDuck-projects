4cc8b76ddb3195972a665536b4ea7a8617ffd05e
Hongnan Gao <reighns.sjr.sjh.gr.twt.hrj@gmail.com>
2022-05-14 17:11:48 +0800

    Refactor: clean up codes and docstrings in detector.py.

--- custom_hn_melanoma_gradcam/src/custom_nodes/model/resnets/resnet_files/detector.py
+++ custom_hn_melanoma_gradcam/src/custom_nodes/model/resnets/resnet_files/detector.py
@@ -1,5 +1,6 @@
 import logging
 from pathlib import Path
+from typing import Any, Dict, Union
 
 import albumentations
 import matplotlib.pyplot as plt
@@ -9,24 +10,23 @@ from albumentations.pytorch.transforms import ToTensorV2
 from pytorch_grad_cam import GradCAM
 from pytorch_grad_cam.utils.image import show_cam_on_image
 
-from typing import Dict, Any, List
-
 from custom_hn_melanoma_gradcam.src.custom_nodes.model.resnets.resnet_files import (
     model,
 )
 
 
 class Detector:  # pylint: disable=too-few-public-methods, too-many-instance-attributes, not-callable
-    """Object detection class using YOLOX to predict object bboxes.
+    """Image Classification Model ResNet used to predict Melanoma.
 
     Attributes:
         logger (logging.Logger): Events logger.
-        config (Dict[str, Any]): YOLOX node configuration.
+        config (Dict[str, Any]): ResNet node configuration.
         model_dir (pathlib.Path): Path to directory of model weights files.
         device (torch.device): Represents the device on which the torch.Tensor
             will be allocated.
         half (bool): Flag to determine if half-precision should be used.
-        yolox (YOLOX): The YOLOX model for performing inference.
+        transform_image (albumentations.Compose): Albumentations Compose object.
+        resnet50d (model.CustomNeuralNet): The ResNet model for performing inference.
     """
 
     def __init__(self, config: Dict[str, Any], model_dir: Path) -> None:
@@ -46,51 +46,72 @@ class Detector:  # pylint: disable=too-few-public-methods, too-many-instance-att
 
         self.resnet50d = self._create_resnet50d_model()
 
-    def _get_model(self):
-        """Constructs YOLOX model based on parsed configuration.
-
-        Args:
-            model_size (Dict[str, float]): Depth and width of the model.
-
-        Returns:
-            (YOLOX): YOLOX model.
-        """
-        return model.CustomNeuralNet(
-            model_name=self.config["model_params"]["model_name"],
-            out_features=self.config["model_params"]["out_features"],
-            in_channels=self.config["model_params"]["in_channels"],
-            pretrained=self.config["model_params"]["pretrained"],
-            use_meta=self.config["model_params"]["use_meta"],
-        )
-
     # the latest version uses @torch.inference_mode(mode=True)
     @torch.no_grad()
-    def predict_class_from_image(self, image):
-        image = self.transform_image(image=image)["image"].to(self.device)
-        if len(image.shape) != 4:
-            image = image.unsqueeze(0)
-        logits = self.resnet50d(image)
-        probs = getattr(torch.nn, "Softmax")(dim=1)(logits).cpu().numpy()
-        class_name = self.class_label_map[np.argmax(probs)]
-        return {"pred_label": class_name, "pred_score": probs}
+    def predict_class_from_image(
+        self, image: np.ndarray
+    ) -> Union[Dict[str, str], Dict[str, float]]:
+        """Predicts the class of an image.
 
-    def _create_resnet50d_model(self):
+        Note:
+            Our pipeline takes in input as np.ndarray but inference needs to be done as torch.Tensor.
+            Albumentations handles this by converting the np.ndarray to torch.Tensor.
+            Note we use max to get pred_score, so it corresponds to the probability of the predicted class.
+
+        Args:
+            image (np.ndarray): The input image.
+
+        Returns:
+            Union[Dict[str, str], Dict[str, float]]:
+                - pred_label (str): The predicted class label.
+                - pred_score (float): The predicted class score.
+        """
+
+        image = self.transform_image(image=image)["image"].to(self.device)
+
+        if len(image.shape) != 4:
+            image = image.unsqueeze(0)  # np.expand_dims(image, 0)
+
+        logits = self.resnet50d(image)  # model.forward(image)
+        # only 1 image per inference take 0 index.
+        probs = getattr(torch.nn, "Softmax")(dim=1)(logits).cpu().numpy()[0]
+        pred_score = probs.max() * 100
+        class_name = self.class_label_map[np.argmax(probs)]
+        return {"pred_label": class_name, "pred_score": pred_score}
+
+    def _create_resnet50d_model(self) -> model.CustomNeuralNet:
+        """Creates ResNet50d model and loads its weights.
+
+        Returns:
+            (model.CustomNeuralNet): ResNet50d model.
+        """
         model_type = self.config["model_params"]["model_name"]
         model_path = (
             self.model_dir / self.config["weights"]["model_file"][model_type]
         )
         return self._load_resnet50d_weights(model_path)
 
-    def _load_resnet50d_weights(self, model_path: Path):
+    def _get_model(self) -> model.CustomNeuralNet:
+        """Constructs ResNet50d model based on parsed configuration.
 
-        """Loads YOLOX model weights.
+        Args:
+            config["model_params"] (Dict[str, Any]): Model parameters from config file to be unpacked.
+
+        Returns:
+            (model.CustomNeuralNet): ResNet50d model.
+        """
+        return model.CustomNeuralNet(**self.config["model_params"])
+
+    def _load_resnet50d_weights(
+        self, model_path: Path
+    ) -> model.CustomNeuralNet:
+        """Loads ResNet50d model weights.
 
         Args:
             model_path (Path): Path to model weights file.
-            model_settings (Dict[str, float]): Depth and width of the model.
 
         Returns:
-            (YOLOX): YOLOX model.
+            model (model.CustomNeuralNet): ResNet50d model.
 
         Raises:
             ValueError: `model_path` does not exist.
@@ -104,23 +125,24 @@ class Detector:  # pylint: disable=too-few-public-methods, too-many-instance-att
                 model.half()
             model.eval()
             model.load_state_dict(ckpt["model_state_dict"])
-            print(f"Loaded model weights from {model_path}")
+            self.logger.info(f"Loaded model weights from {model_path}")
+
             return model
 
         raise ValueError(
             f"Model file does not exist. Please check that {model_path} exists."
         )
 
-    def _get_transforms(self):
-        """Performs Augmentation on test dataset.
-        Remember tta transforms need resize and normalize.
+    def _get_transforms(self) -> albumentations.Compose:
+        """Gets the transforms function for the image.
+
         Args:
-            pipeline_config (global_params.PipelineConfig): The pipeline config.
-            image_size (int, optional): The image size. Defaults to TRANSFORMS.image_size.
-            mean (List[float], optional): The mean. Defaults to TRANSFORMS.mean.
-            std (List[float], optional): The std. Defaults to TRANSFORMS.std.
+            image_size (int, optional): The image size. Defaults to config["input_size"].
+            mean (List[float], optional): The mean. Defaults to config["mean"].
+            std (List[float], optional): The std. Defaults to config["std"].
+
         Returns:
-            transforms_dict (Dict[str, albumentations.core.composition.Compose]): Returns the transforms for inference in a dictionary which can hold TTA transforms.
+            albumentations.Compose: An Albumentations Compose object.
         """
 
         return albumentations.Compose(
@@ -140,10 +162,22 @@ class Detector:  # pylint: disable=too-few-public-methods, too-many-instance-att
         )
 
     def show_resnet_gradcam(
-        self, image: np.ndarray, original_image, plot_gradcam=True
-    ):
-        """Log gradcam images into wandb for error analysis.
-        # TODO: Consider getting the logits for error analysis, for example, if a predicted image which is correct has high logits this means the model is very sure, conversely, if a predicted image has low logits and also wrong, we also check why.
+        self,
+        image: np.ndarray,
+        original_image: np.ndarray,
+        plot_gradcam: bool = True,
+    ) -> np.ndarray:
+        """Show the gradcam of the image for ResNet variants.
+
+        This will not work on other types of network architectures.
+
+        Args:
+            image (np.ndarray): The input image.
+            original_image (np.ndarray): The original image.
+            plot_gradcam (bool): If True, will plot the gradcam. Defaults to True.
+
+        Returns:
+            gradcam_image (np.ndarray): The gradcam image.
         """
         model = self.resnet50d
         # only for resnet variants! for simplicity we don't enumerate other models!
