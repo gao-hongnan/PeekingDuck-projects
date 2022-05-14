70edc2eac8f2082aafbf34fd82a777ef8ac75e37
Hongnan Gao <reighns.sjr.sjh.gr.twt.hrj@gmail.com>
2022-05-14 17:14:32 +0800

    Refactor: clean up code and docstrings in resnets/resnet_files/model.py.

--- custom_hn_melanoma_gradcam/src/custom_nodes/model/resnets/resnet_files/model.py
+++ custom_hn_melanoma_gradcam/src/custom_nodes/model/resnets/resnet_files/model.py
@@ -7,17 +7,19 @@ class CustomNeuralNet(torch.nn.Module):
     def __init__(
         self,
         model_name: str,
-        out_features: int,
         in_channels: int,
+        out_features: int,
         pretrained: bool,
         use_meta: bool,
-    ):
-        """Construct a new model.
+    ) -> None:
+        """Construct a CustomNeuralNet model from timm.
+
         Args:
-            model_name ([type], str): The name of the model to use. Defaults to MODEL_PARAMS.model_name.
-            out_features ([type], int): The number of output features, this is usually the number of classes, but if you use sigmoid, then the output is 1. Defaults to MODEL_PARAMS.output_dimension.
-            in_channels ([type], int): The number of input channels; RGB = 3, Grayscale = 1. Defaults to MODEL_PARAMS.input_channels.
-            pretrained ([type], bool): If True, use pretrained model. Defaults to MODEL_PARAMS.pretrained.
+            model_name (str): The name of the model to use.
+            in_channels (int): The number of input channels; RGB = 3, Grayscale = 1.
+            out_features (int): The number of output features, this is usually the number of classes, but if you use sigmoid, then the output is 1.
+            pretrained (bool): If True, use pretrained model.
+            use_meta (bool): If True, use meta-data.
         """
         super().__init__()
 
@@ -29,7 +31,7 @@ class CustomNeuralNet(torch.nn.Module):
             model_name, pretrained=self.pretrained, in_chans=self.in_channels
         )
 
-        # removes head from backbone: # TODO: Global pool = "avg" vs "" behaves differently in shape, caution!
+        # removes head from backbone: Global pool = "avg" vs "" behaves differently in shape, caution!
         self.backbone.reset_classifier(num_classes=0, global_pool="avg")
 
         # get the last layer's number of features in backbone (feature map)
@@ -53,19 +55,23 @@ class CustomNeuralNet(torch.nn.Module):
     def extract_features(self, image: torch.FloatTensor) -> torch.FloatTensor:
         """Extract the features mapping logits from the model.
         This is the output from the backbone of a CNN.
+
         Args:
             image (torch.FloatTensor): The input image.
+
         Returns:
             feature_logits (torch.FloatTensor): The features logits.
         """
-        # TODO: To rename feature_logits to image embeddings, also find out what is image embedding.
+
         feature_logits = self.architecture["backbone"](image)
         return feature_logits
 
     def forward(self, image: torch.FloatTensor) -> torch.FloatTensor:
         """The forward call of the model.
+
         Args:
             image (torch.FloatTensor): The input image.
+
         Returns:
             classifier_logits (torch.FloatTensor): The output logits of the classifier head.
         """
