b698ccd186bb9c058b924345951aea52d9fe1f55
Hongnan G. <reighns92@users.noreply.github.com>
2022-09-09 02:54:36 +0000

    Deploying bf0a612be5ddea5a8b7866e82556eb4c6243581a

---
+++ javascript/mathjax.js
@@ -0,0 +1,19 @@
+window.MathJax = {
+    tex: {
+        inlineMath: [["\\(", "\\)"]],
+        displayMath: [["\\[", "\\]"]],
+        processEscapes: true,
+        processEnvironments: true
+    },
+    options: {
+        ignoreHtmlClass: ".*|",
+        processHtmlClass: "arithmatex"
+    }
+};
+
+document$.subscribe(() => {
+    This integrates MathJax with instant loading.
+
+
+    MathJax.typesetPromise()
+});
